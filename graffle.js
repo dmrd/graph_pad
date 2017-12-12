d0c9648589a8a81ce13d09c80d49cc04ff505a32
David Dohan <dev@ddohan.com>
2017-12-11 19:01:03 -0800

    Separate defining new shape examples

--- graffle.js
+++ graffle.js
@@ -60,32 +60,15 @@ var dragger = function () {
     this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
     this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
     this.animate({"fill-opacity": .2}, 500);
-},
-    move = function (dx, dy) {
+}
+var move = function (dx, dy) {
         var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
         this.attr(att);
         for (var i = connections.length; i--;) {
             r.connection(connections[i]);
         }
-    },
-    up = function () {
+}
+var up = function () {
         this.animate({"fill-opacity": 0}, 500);
         can_draw = true;
-    },
-    // r = Raphael("graph_viz", 640, 480),
-    r = sketchpad.paper()
-    connections = [],
-    shapes = [  r.ellipse(190, 100, 30, 20),
-                r.rect(290, 80, 60, 40, 10),
-                r.rect(290, 180, 60, 40, 2),
-                r.ellipse(450, 100, 20, 20)
-            ];
-for (var i = 0, ii = shapes.length; i < ii; i++) {
-    var color = Raphael.getColor();
-    shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-    shapes[i].drag(move, dragger, up);
-}
-connections.push(r.connection(shapes[0], shapes[1], "#fff"));
-connections.push(r.connection(shapes[1], shapes[2], "#fff", "#fff|5"));
-connections.push(r.connection(shapes[1], shapes[3], "#000", "#fff"));
-connections.push(r.connection(shapes[1], shapes[2], "#000", "#fff"));
+    }
